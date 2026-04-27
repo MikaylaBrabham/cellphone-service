80d1573fa555324f5a26aad2fd33510003d5bc87
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-27 14:43:10 -0500

    got getters and setters

--- src/main/java/com/pluralsight/CellphoneService.java
+++ src/main/java/com/pluralsight/CellphoneService.java
@@ -1,11 +1,8 @@
 package com.pluralsight;
-
+//import scanner
 import java.util.Scanner;
 
 public class CellphoneService {
-    //import scanner
-
-
     public static void main(String[] args) {
 
     }
@@ -19,17 +16,66 @@ public class CellphoneService {
 
     // added cellphone class to instantiate objects in
     class CellPhone {
-        //add scanner to get user info
-       // Scanner scanner = new Scanner(System.in);
-        //add cellphoen details needed properties/ data members
-       CellPhone cellDetails = new CellPhone();
-       CellPhone(cellDetails  Double serialNumber, String model, String carrier, String phoneNumber, String owner){
-           serialNumber = 0;
-           model  = "";
-           carrier = "";
-           phoneNumber = "";
-           owner = "";
-       }
+
+        //add cellphone details needed properties/ data members
+        Double serialNumber;
+        String model ;
+        String carrier;
+        String phoneNumber;
+        String owner;
+ // provide getter and setters for all 5 data memebers
+        public Double getSerialNumber() {
+            return serialNumber;
+        }
+
+        public void setSerialNumber(Double serialNumber) {
+            this.serialNumber = serialNumber;
+        }
+
+        public String getModel() {
+            return model;
+        }
+
+        public void setModel(String model) {
+            this.model = model;
+        }
+
+        public String getCarrier() {
+            return carrier;
+        }
+
+        public void setCarrier(String carrier) {
+            this.carrier = carrier;
+        }
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getOwner() {
+            return owner;
+        }
+
+        public void setOwner(String owner) {
+            this.owner = owner;
+        }
+
+        //add empty parameterless constructor from generate
+        public CellPhone(Double serialNumber, String model, String carrier, String phoneNumber, String owner) {
+            this.serialNumber = serialNumber = 0.0;
+            this.model = model = "";
+            this.carrier = carrier = "";
+            this.phoneNumber = phoneNumber = "";
+            this.owner = owner = "";
+        }
+
+
+
+
 
 
 
