538c62308dd7cfde555dfb0795987262d2628963
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-27 14:00:15 -0500

    i was doing this wrong i just needed to declare the variables properties

--- src/main/java/com/pluralsight/CellphoneService.java
+++ src/main/java/com/pluralsight/CellphoneService.java
@@ -1,19 +1,37 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class CellphoneService {
+    //import scanner
+
+
     public static void main(String[] args) {
 
     }
 
 
     // created a cellphone application class with a main
-    public class CellPhoneApplication {
+    class CellPhoneApplication {
         public static void main(String[] args) {
         }
     }
 
     // added cellphone class to instantiate objects in
-    public class CellPhone {
+    class CellPhone {
+        //add scanner to get user info
+       // Scanner scanner = new Scanner(System.in);
+        //add cellphoen details needed properties/ data members
+       CellPhone cellDetails = new CellPhone();
+       CellPhone(cellDetails  Double serialNumber, String model, String carrier, String phoneNumber, String owner){
+           serialNumber = 0;
+           model  = "";
+           carrier = "";
+           phoneNumber = "";
+           owner = "";
+       }
+
+
 
     }
 }
