60739238c2fa0e980208bbd512e44d72bfe6579c
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-27 13:35:14 -0500

    created cellphone class and cellphone app class - just learned the whoe class needs to encapsulate everything so the one auto made will encapsulate the enitre project

--- src/main/java/com/pluralsight/CellphoneService.java
+++ src/main/java/com/pluralsight/CellphoneService.java
@@ -4,4 +4,16 @@ public class CellphoneService {
     public static void main(String[] args) {
 
     }
+
+
+    // created a cellphone application class with a main
+    public class CellPhoneApplication {
+        public static void main(String[] args) {
+        }
+    }
+
+    // added cellphone class to instantiate objects in
+    public class CellPhone {
+
+    }
 }
